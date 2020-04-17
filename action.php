ae5cb34ce80028fe1e929a4fb137ce1f8e7bc541
Grégory Mage <gregory.mage@gmail.com>
2020-04-17 15:17:56 +0200

    petite correction

--- action.php
+++ action.php
@@ -171,7 +171,7 @@ if ($op == 'clone' || $op == 'edit' || $op == 'del' || $op == 'add' || $op == 'l
 						//xmdoc
 						if (xoops_isActiveModule('xmdoc') && $helper->getConfig('general_xmdoc', 0) == 1) {
 							xoops_load('utility', 'xmdoc');
-							$error_message .= XmdocUtility::delDocdata('xmarticle', $article_id);
+							XmdocUtility::delDocdata('xmarticle', $article_id);
 						}                        
 						//Del Notification and comment
 						$helper = \Xmf\Module\Helper::getHelper('xmarticle');
